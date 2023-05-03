fbbe8fc3088f6619d411ad19a7cec1cfd0050b2a
mszkudla <259248@student.pwr.edu.pl>
2023-05-03 17:39:36 +0200

    Layout changes

--- Models/Album.cs
+++ Models/Album.cs
@@ -3,9 +3,7 @@
     public class Album
     {
         public int Id { get; set; }
-
         public string AlbumName { get; set; } = null!;
-
         public virtual ICollection<UserAlbum> UserAlbums { get; set; } = null!;
         public virtual ICollection<AlbumSong> AlbumSongs { get; set; } = null!;
     }
--- Models/AlbumSong.cs
+++ Models/AlbumSong.cs
@@ -3,7 +3,6 @@
 public class AlbumSong
 {
     public int Id { get; set; }
-
     public int SongId { get; set; }
     public int AlbumId { get; set; }
     public virtual Song Song { get; set; } = null!;
